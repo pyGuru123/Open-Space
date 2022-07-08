f009f26c0a315cfc2b671d6b34d00dd55b421346
pyguru123 <pyguru123@github.com>
2022-07-08 13:13:52 +0530

    removed neows

--- src/scripts/epic.js
+++ src/scripts/epic.js
@@ -1,26 +1,50 @@
 const imagery = document.querySelector('.imagery');
 const datepicker = document.querySelector('.datepick');
 
-function updateImage(src) {
-	imagery.src = "https://epic.gsfc.nasa.gov/archive/enhanced/2022/06/30/png/epic_RGB_20220630011358.png";
-}
+// variable declarations;
+
+let imgarr = [];
+let apikey = "4dbQbZnGMVLS5g3SEr094513VGLJnqAjuDEMlYx3";
 
 window.onload = function() {
 	updateImage("");
+	fetchImages();
 }
 
 todayDate = getDate();
 datepicker.value = todayDate;
 datepicker.setAttribute('max', todayDate);
 
-function getDate(offset=0) {
+datepicker.addEventListener("change", fetchImages);
+
+function getDate(offset=7) {
 	var today = new Date();
 	if (offset) {
 		newdate = new Date();
-		today.setDate(newdate.getDate()-1);
+		today.setDate(newdate.getDate()-offset);
 	}
 	var date = String(today.getDate()).padStart(2, '0');
 	var month = String(today.getMonth() + 1).padStart(2, '0');
 	var year = today.getFullYear();
 	return year + '-' + month + '-' + date;
 }
+
+function updateImage(src) {
+	imagery.src = "https://epic.gsfc.nasa.gov/archive/enhanced/2022/06/30/png/epic_RGB_20220630011358.png";
+}
+
+function fetchImages() {
+	todayDate = datepicker.value;
+	let endpoint = `https://api.nasa.gov/EPIC/api/natural/date/${todayDate}?api_key=${apikey}`;
+
+	fetch(endpoint)
+	.then (response => response.json())
+	.then (data => printData(data))
+}
+
+function printData(data) {
+	console.log(data);
+}
+
+
+// function ImageList
